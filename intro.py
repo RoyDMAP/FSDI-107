40d3e453e847593e0f489072a0bc43f72ef5412c
RoyDMAP <roy.dimapilis@sdgku.edu>
2024-07-29 17:04:06 -0700

    first commit

---
+++ intro.py
@@ -0,0 +1,45 @@
+print("Hello world")
+
+#variables
+name = "Bruce"
+last_name = "Wayne"
+age = 100
+isAvailable = True
+
+print(name)
+print(last_name)
+print(age)
+print(isAvailable)
+
+# if statement
+if age < 100:
+    print("You are younger than 100")
+elif age == 100:
+    print("You are 100 years old")
+else:
+    print("You are older than 100")
+
+print("I'm outside")
+
+
+def say_hi():
+    print('Hello there!')  
+
+def say_good_bye():
+    print("Goodbye!") 
+
+#math
+print(1 + 2)
+print(8 - 4)
+print(20 * 5)
+print(100 / 10)
+print("5 + 2 = " + {5+2})
+
+if age <= 20:
+    print("message")
+elif age == 20:
+   print("message")
+else:
+    print("message")
+
+
---
+++ test.py
@@ -0,0 +1,2 @@
+# Assignment
+print("Hello world test.py")
